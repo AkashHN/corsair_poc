f29b09e3244db01e1c73dfbc8efca30ced959011
akashngowda@outlook.com <akashngowda@outlook.com>
2024-03-08 17:22:47 +0530

    Updated actions.yml file

--- cucumber.report.js
+++ cucumber.report.js
@@ -6,9 +6,10 @@ const reporter = require('cucumber-html-reporter');
     const open = openModule.default;
  
     const options = {
-      jsonFile: './reports/json/cucumber_report.json',
+      jsonFile: 'reports/json/',
       launchReport: false,
       noInlineScreenshots: false,
+      screenshotsDirectory:"reports/screenshots/",
       output: 'reports/cucumber-report.html',
       reportSuiteAsScenarios: true,
       scenarioTimestamp: true,
@@ -18,11 +19,6 @@ const reporter = require('cucumber-html-reporter');
  
     // Generate HTML report
     reporter.generate(options);
- 
-    console.log('HTML report generated successfully: reports/cucumber-report.html');
- 
-    // Open the generated report in the default browser
-    await open('reports/cucumber-report.html');
   } catch (error) {
     console.error('Error occurred:', error);
   }
